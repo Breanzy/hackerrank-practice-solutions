4b29ea0da660f30425be1263a0ed4a4f853c6139
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2025-02-10 23:31:23 +1100

    First problem

---
+++ 30-Days-Of-Code/Solutions/0-Javascript.js
@@ -0,0 +1,10 @@
+//basic parameter function variable usage
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+
+    //input variable here to return
+    console.log(inputString);
+    }
+
+
