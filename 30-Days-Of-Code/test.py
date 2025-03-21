06a7795fe596027d40ee355f3db5ffdc808c152d
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2025-03-21 23:24:53 +1100

    temp commit

--- 30-Days-Of-Code/test.py
+++ 30-Days-Of-Code/test.py
@@ -1,15 +1,1 @@
-def addition(x, y):
-    return x + y
-
-def subtraction(x, y):
-    return x + y
-
-def multiplication(x, y):
-    return x + y
-
-def division(x, y):
-    return x + y
-
-input("Select value to use"):
-
-testt
+print("Hello World")
